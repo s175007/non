fbf3f24512bc5add153782b8e4cffbe4308c6838
Ami <s175007@nishitech.ac.jp>
2021-02-09 01:01:28 +0900

    update

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,61 +1,54 @@
-
 @extends('layouts.header')
 
 @section('content')
-<body>
 
-    @foreach ($food_types as $food_type)
-        <h2 style="">{{ $food_type->name }}</h2>
-    <section class=" slider">
-            <ul id="food-{{ $loop->index }}" class="cs-hidden">
-                @forelse ($food_type->foods as $food)
+    <body>
+
+        @foreach ($food_types as $food_type)
+            <h2 style="">{{ $food_type->name }}</h2>
+            <section class=" slider">
+                <ul id="food-{{ $loop->index }}" class="cs-hidden">
+                    @forelse ($food_type->foods as $food)
 
 
-                    <li class="item-a">
-                        <!--box-slider--------------->
-                        <div class="box">
-                            <!--img-box---------->
-                            <div class="slide-img">
-                                <img alt="1" src="{{ asset ('upload/'.$food->image)}}">
-                                <!--overlayer---------->
-                                {{-- <div class="overlay">
+                        <li class="item-a">
+                            <!--box-slider--------------->
+                            <div class="box">
+                                <!--img-box---------->
+                                <div class="slide-img">
+                                    <img alt="1" src="{{ asset('upload/' . $food->image) }}">
+                                    <!--overlayer---------->
+                                    {{-- <div class="overlay">
                                     <!--buy-btn------>
                                     <a href="#" class="buy-btn">Buy Now</a>
                                 </div> --}}
-                            </div>
-                            <!--detail-box--------->
-                            @if ($food->status)
-                            <div class="detail-box">
-                                <!--type-------->
-                                <div class="type">
-                                    <a href="">{{ $food->foodnamevn }}</a>
-                                    <span>{{ $food->foodnamejp }}</span>
                                 </div>
-                                <!--price-------->
-                                <a href="" class="price">{{ $food->price }}円</a>
-
-                            </div>
-                            @else
-                            <div class="detail-box">
-                                <div class="type">
-                                    <a href="">{{ $food->foodnamevn }}</a>
-                                    <span>{{ $food->foodnamejp }}</span>
+                                <!--detail-box--------->
+                                <div class="detail-box">
+                                    <!--type-------->
+                                    <div class="type">
+                                        <a href="">{{ $food->foodnamevn }}</a>
+                                        <span>{{ $food->foodnamejp }}</span>
+                                    </div>
+                                    <!--price-------->
+                                    @if ($food->status)
+                                        <a href="" class="price">{{ $food->price }}円</a>
+                                    @else
+                                        <a style="color: #d2321e">Hết</a>
+                                    @endif
                                 </div>
-                                <!--price-------->
-                                <a style="color: #d2321e">Hết</a>
                             </div>
-                            @endif
-                        </div>
-                    </li>
-                @empty
-                    <p>.</p>
-    @endforelse
-    </ul>
-    </section>
-    @endforeach
+                        </li>
+                    @empty
+                        <p>.</p>
+        @endforelse
+        </ul>
+        </section>
+        @endforeach
 
-    <script>
-        let food_types_count = {{count($food_types)}};
-    </script>
-</body>
+        <script>
+            let food_types_count = {{ count($food_types) }};
+
+        </script>
+    </body>
 @endsection
